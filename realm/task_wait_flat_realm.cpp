26239dffcb3439caa056aea52e0fc1a69f6cb42a
Max Grossman <jmaxg3@gmail.com>
2017-03-05 09:58:59 -0800

    Fix Realm flat wait benchmark

--- realm/task_wait_flat_realm.cpp
+++ realm/task_wait_flat_realm.cpp
@@ -4,13 +4,30 @@
 #include <stdio.h>
 #include "task_wait_flat.h"
 
+static Realm::Processor aggregateCpu;
+
 enum {
-    TASK_ID = Realm::Processor::TASK_ID_FIRST_AVAILABLE + 0
+    TASK_ID = Realm::Processor::TASK_ID_FIRST_AVAILABLE + 0,
+    DRIVER_ID
 };
 
 static void task(const void *args, size_t arglen, const void *userdata,
         size_t userlen, Realm::Processor p) {
     arglen = arglen + 1;
+}
+
+static void driver_task(const void *args, size_t arglen, const void *userdata,
+        size_t userlen, Realm::Processor p) {
+    const unsigned long long start_time = current_time_ns();
+    int i;
+    for (i = 0; i < N_FLAT_TASK_WAITS; i++) {
+        Realm::Event e = aggregateCpu.spawn(TASK_ID, NULL, 0);
+        e.wait();
+    }
+    const unsigned long long end_time = current_time_ns();
+    printf("METRIC task_wait_flat %d %.20f\n", N_FLAT_TASK_WAITS,
+            (double)N_FLAT_TASK_WAITS / ((double)(end_time -
+                    start_time) / 1000.0));
 }
 
 static int collect_cpus(std::vector<Realm::Processor> &all_cpus) {
@@ -35,20 +52,14 @@ int main(int argc, char **argv) {
     std::vector<Realm::Processor> all_cpus;
     runtime.init(&argc, &argv);
     runtime.register_task(TASK_ID, task);
+    runtime.register_task(DRIVER_ID, driver_task);
 
     int count_cpus = collect_cpus(all_cpus);
+    aggregateCpu = Realm::Processor::create_group(all_cpus);
     printf("Using %d Realm threads\n", count_cpus);
 
-    const unsigned long long start_time = current_time_ns();
-    int i;
-    for (i = 0; i < N_FLAT_TASK_WAITS; i++) {
-        Realm::Event e = all_cpus.at(i % count_cpus).spawn(TASK_ID, NULL, 0);
-        e.external_wait();
-    }
-    const unsigned long long end_time = current_time_ns();
-    printf("METRIC task_wait_flat %d %.20f\n", N_FLAT_TASK_WAITS,
-            (double)N_FLAT_TASK_WAITS / ((double)(end_time -
-                    start_time) / 1000.0));
+    Realm::Event e = aggregateCpu.spawn(DRIVER_ID, NULL, 0);
+    e.external_wait();
 
     runtime.shutdown();
     runtime.wait_for_shutdown();
